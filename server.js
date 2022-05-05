f3f176bfd259bb2d0fe99ee38abea92b8e0a545c
Aditi Mishra <aditi.mishra_cs19@gla.ac.in>
2022-05-05 22:30:53 +0530

    ninth comit

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-    res.render('home')
-
-})
 
 // set template
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    res.render('home')
+
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
