7e413c079617b9e92fffd9ced054e8b9286f28da
Aditi Mishra <aditi.mishra_cs19@gla.ac.in>
2022-05-16 00:10:35 +0530

    twentythird comit

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -9,6 +9,14 @@ function authController() {
             res.render('auth/login')
         },
         postLogin(req, res, next) {
+            const { email, password } = req.body
+
+            //validation request
+            if (!email || !password) {
+                req.flash('error', 'All fields are required')
+                req.flash('email', email)
+                return res.redirect('/login')
+            }
             passport.authenticate('local', (err, user, info) => {
                 if (err) {
                     req.flash('error', info.message)
